d326acf993f69acb7747606a1de110a39a0f6a1d
Samuel Bell <samueljamesbell@gmail.com>
2018-04-18 16:56:08 +0100

    Finish applying corrections cases

--- correct.py
+++ correct.py
@@ -1,24 +1,6 @@
 import argparse
 
-
-def apply_corrections(sentence, corrections):
-    """Return a new sentence with corrections applied.
-
-    Sentence should be a whitespace-separated tokenised string. Corrections
-    should be a list of corrections.
-    """
-    tokens = sentence.split(' ')
-
-    for c in corrections:
-        tokens = _apply_correction(tokens, c)
-
-    return ' '.join(tokens)
-
-
-def _apply_correction(tokens, correction):
-    """Apply a single correction to a list of tokens."""
-    start_token_offset, end_token_offset, _, to_insert = correction
-    return tokens[:start_token_offset] + to_insert + tokens[end_token_offset:]
+import parser
 
 
 def _parse_args():
@@ -27,9 +9,50 @@ def _parse_args():
     return parser.parse_args()
 
 
+def apply_corrections(sentence, corrections):
+    """Return a new sentence with corrections applied.
+
+    Sentence should be a whitespace-separated tokenised string. Corrections
+    should be a list of corrections.
+    """
+    tokens = sentence.split(' ')
+    offset = 0
+
+    for c in corrections:
+        tokens, offset = _apply_correction(tokens, c, offset)
+
+    return ' '.join(tokens)
+
+
+def _apply_correction(tokens, correction, offset):
+    """Apply a single correction to a list of tokens."""
+    start_token_offset, end_token_offset, _, insertion = correction
+    to_insert = insertion[0].split(' ')
+    end_token_offset += (len(to_insert) - 1)
+
+    to_insert_filtered = [t for t in to_insert if t != '']
+
+    head = tokens[:start_token_offset + offset]
+    tail = tokens[end_token_offset + offset:]
+
+    new_tokens = head + to_insert_filtered + tail
+
+    new_offset = len(to_insert_filtered) - (end_token_offset - start_token_offset) + offset
+
+    return new_tokens, new_offset
+
+
 def _main():
     args = _parse_args()
-    print(args)
+
+    with open(args.path, 'r', encoding='utf-8') as f:
+        lines = f.read().split('\n')
+
+    sentences, corrections = parser.parse(lines)
+
+    for s, c in zip(sentences, corrections):
+        corrected = apply_corrections(s, c[0])
+        print(corrected)
 
 
 if __name__ == '__main__':
--- test_correct.py
+++ test_correct.py
@@ -18,3 +18,82 @@ class CorrectSentenceTest(unittest.TestCase):
         expected = 'In the modern digital world .'
         actual = correct.apply_corrections(sentence, corrections)
         self.assertEqual(expected, actual)
+
+    def test_single_replace(self):
+        sentence = ('In modern digital world , electronic products are '
+                    'widely used in daily lives such as Smart phones , '
+                    'computers and etc .')
+        corrections = [(12, 13, 'lives', ['life'])]
+        expected = ('In modern digital world , electronic products are '
+                    'widely used in daily life such as Smart phones , '
+                    'computers and etc .')
+        actual = correct.apply_corrections(sentence, corrections)
+        self.assertEqual(expected, actual)
+                   
+    def test_replace_and_insert(self):
+        sentence = ('In modern digital world , electronic products are '
+                    'widely used in daily lives such as Smart phones , '
+                    'computers and etc .')
+        corrections = [(1, 1, '', ['the']), (12, 13, 'lives', ['life'])]
+        expected = ('In the modern digital world , electronic products are '
+                    'widely used in daily life such as Smart phones , '
+                    'computers and etc .')
+        actual = correct.apply_corrections(sentence, corrections)
+        self.assertEqual(expected, actual)
+
+    def test_single_reduce(self):
+        sentence = ('In work places , electronic devices '
+                    'such as computers are also inevitable to '
+                    'use to increase the productivity of the corporation .')
+        corrections = [(1, 2, u'work', [u'the workplace'])]
+        expected = ('In the workplace , electronic devices '
+                    'such as computers are also inevitable to '
+                    'use to increase the productivity of the corporation .')
+        actual = correct.apply_corrections(sentence, corrections)
+        self.assertEqual(expected, actual)
+
+    def test_reduce_and_insert(self):
+        sentence = ('In work places , electronic devices '
+                    'such as computers are also inevitable to '
+                    'use to increase the productivity of the corporation .')
+        corrections = [(1, 2, u'work', [u'the workplace']), (6, 6, '', [u','])]
+        expected = ('In the workplace , electronic devices , '
+                    'such as computers are also inevitable to '
+                    'use to increase the productivity of the corporation .')
+        actual = correct.apply_corrections(sentence, corrections)
+        self.assertEqual(expected, actual)
+           
+    def test_reduce_insert_insert_reduce(self):
+        sentence = ('In work places , electronic devices '
+                    'such as computers are also inevitable to '
+                    'use to increase the productivity of the corporation .')
+        corrections = [
+                (1, 2, u'work', [u'the workplace']),
+                (6, 6, '', [u',']),
+                (9, 9, '', [u',']),
+                (13, 15, u'use to', [u''])
+        ]
+        expected = ('In the workplace , electronic devices , '
+                    'such as computers , are also inevitable to '
+                    'increase the productivity of the corporation .')
+        actual = correct.apply_corrections(sentence, corrections)
+        self.assertEqual(expected, actual)
+ 
+    def test_difficult_example(self):
+        sentence = ('Surveillance technology such as RFID '
+                    'can be operated twenty-four hours with the '
+                    'absence of operators to track done every detail '
+                    'about human activities .')
+        corrections = [
+            (2, 2, '', [u',']),
+            (5, 5, '', [u',']),
+            (10, 11, u'with', [u'without']),
+            (11, 14, u'the absence of', [u'']),
+            (17, 18, u'done', [u''])
+        ]
+        expected = ('Surveillance technology , such as RFID , '
+                    'can be operated twenty-four hours without '
+                    'operators to track every detail '
+                    'about human activities .')
+        actual = correct.apply_corrections(sentence, corrections)
+        self.assertEqual(expected, actual)
